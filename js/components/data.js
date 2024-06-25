fd3a9924afb728323add46e3a7c64c2a0cd6be0c
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-06-25 15:27:47 -0500

    feat: :hammer: creare the data of data.js file

---
+++ js/components/data.js
@@ -0,0 +1,21 @@
+const resultList = document.getElementById("result");
+
+const users = [
+    { avatar: 'avatar_url1', name_full: 'Name User 1', desciption: 'Description Users 1' },
+    { avatar: 'avatar_url2', name_full: 'Name User 2', desciption: 'Description Users 2' }
+];
+
+export const getUsers = ()=> {
+    resultList.innerHTML = users.map(user =>
+        /*html*/
+        `<li>
+            <img src="${user.avatar}">
+            <div class="user_info">
+                <h4>${user.name_full}</h4>
+                <p>${user.description}</p>
+            </div>
+        </li>`
+    ).join('');
+}
+
+getUsers();
